03d64df90016fbc854386bbcb59d6e9760e0c3fc
anyatar <anya.suraski@gmail.com>
2024-05-06 19:16:05 +0300

    removed node modules

--- build/src/controllers/home.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.welcome = void 0;
-function welcome(req, res) {
-    console.log("Welcome to VIM API application");
-    return res.json({ message: "Welcome to VIM API application!" });
-}
-exports.welcome = welcome;
-//# sourceMappingURL=home.controller.js.map
