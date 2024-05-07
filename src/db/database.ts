a1e5f2cb2feadacf95a6ecec2caaf1948c1f0dd5
anyatar <anya.suraski@gmail.com>
2024-05-07 12:42:31 +0300

    updated

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,5 +1,27 @@
 import Doctor from "../interfaces/doctor.model";
+import fs from 'fs';
 
 // Mock data (replace with your actual data source)
-const providers:Doctor[] = require('./../../providers.json');
+const providers:Doctor[] = JSON.parse(fs.readFileSync('providers.json', 'utf-8'));
+/*
+const readProvidersData = (filePath: string): Promise<any[]> => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                try {
+                    const parsedData = JSON.parse(data);
+                    resolve(parsedData);
+                } catch (parseError) {
+                    reject(parseError);
+                }
+            }
+        });
+    });
+};
+*/
+
+const cache: { [key: string]: string[] } = {};
+
 export default providers;
