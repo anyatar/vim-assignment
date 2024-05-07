7d39a24a2545c701b51d7e4a394bea0bda09a327
anyatar <anya.suraski@gmail.com>
2024-05-07 12:47:18 +0300

    updated

--- src/db/database.ts
+++ src/db/database.ts
@@ -3,6 +3,23 @@ import fs from 'fs';
 
 // Mock data (replace with your actual data source)
 const providers:Doctor[] = JSON.parse(fs.readFileSync('providers.json', 'utf-8'));
+
+// Create cache of providers by name
+const providersByName = providers.reduce((acc: any, provider: Doctor) => {
+    acc[provider.name] = provider;
+    return acc;
+}, {});
+
+// Create cache of providers by specialty
+/*const providersBySpecialty = providers.reduce((acc:any, provider: Doctor) => {
+    //const specialtyKey = provider.specialties.toLowerCase();
+    if (!acc[specialtyKey]) {
+        acc[specialtyKey] = [];
+    }
+    acc[specialtyKey].push(provider);
+    return acc;
+}, {});*/
+
 /*
 const readProvidersData = (filePath: string): Promise<any[]> => {
     return new Promise((resolve, reject) => {
